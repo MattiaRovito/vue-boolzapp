8c119703653123d5d4f9a4f08a95461bd7f21f3d
MattiaR <rovitomattia91@gmail.com>
2021-06-29 10:46:27 +0200

    correzione v-for chat

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ const app = new Vue ({
         contacts: [
             {
                 name: 'Giangi',
-                avatar: 'img/avatar_4.jpg',
+                avatar: '_4',
                 visible: true,
                 messages: [
                     {
@@ -60,17 +60,17 @@ const app = new Vue ({
                 messages: [
                     {
                     date: '20/03/2020 16:30:00',
-                    text: 'Ciao come stai?',
+                    text: 'Ho fame',
                     status: 'sent'
                     },
                     {
                     date: '20/03/2020 16:30:55',
-                    text: 'Bene grazie! Stasera ci vediamo?',
+                    text: 'Cosa vuoi mangiare stasera?',
                     status: 'received'
                     },
                     {
                     date: '20/03/2020 16:35:00',
-                    text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                    text: 'Pizzaaaa!!',
                     status: 'sent'
                     }
                 ],
@@ -82,17 +82,88 @@ const app = new Vue ({
                 messages: [
                     {
                     date: '20/03/2020 16:30:00',
-                    text: 'Ciao come stai?',
+                    text: 'Cinema stasera?',
                     status: 'sent'
                     },
                     {
                     date: '20/03/2020 16:30:55',
-                    text: 'Bene grazie! Stasera ci vediamo?',
+                    text: 'Cosa proiettano?',
                     status: 'received'
                     },
                     {
                     date: '20/03/2020 16:35:00',
-                    text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                    text: 'Avengers: Endgame',
+                    status: 'sent'
+                    },
+                    {
+                        date: '20/03/2020 16:38:55',
+                        text: 'Sono già lì con i popcorn',
+                        status: 'received'
+                    },
+                ],
+            },
+            {
+                name: 'Brittany',
+                avatar: '_6',
+                visible: true,
+                messages: [
+                    {
+                    date: '20/03/2020 16:30:00',
+                    text: 'Che esercizi abbiamo domani?',
+                    status: 'sent'
+                    },
+                    {
+                    date: '20/03/2020 16:30:55',
+                    text: 'Abbiamo inglese e matematica',
+                    status: 'received'
+                    },
+                    {
+                    date: '20/03/2020 16:35:00',
+                    text: 'Voglio morire',
+                    status: 'sent'
+                    }
+                ],
+            },
+            {
+                name: 'Carl',
+                avatar: '_3',
+                visible: true,
+                messages: [
+                    {
+                    date: '20/03/2020 16:30:00',
+                    text: 'Calcetto stasera!',
+                    status: 'sent'
+                    },
+                    {
+                    date: '20/03/2020 16:30:55',
+                    text: 'No mi dispiace, non posso',
+                    status: 'received'
+                    },
+                    {
+                    date: '20/03/2020 16:35:00',
+                    text: 'Daiii manca il decimo!',
+                    status: 'sent'
+                    }
+                ],
+            },
+            {
+                name: 'El Barba',
+                avatar: '_8',
+                visible: true,
+                messages: [
+                    {
+                    date: '20/03/2020 16:30:00',
+                    text: 'L\'hai sentita l\'ultima dei Greta Van Fleet?',
+                    status: 'sent'
+                    },
+                    {
+                    date: '20/03/2020 16:30:55',
+                    text: 'Dici Heat Above?',
+                    status: 'received'
+                    },
+                    {
+                    date: '20/03/2020 16:35:00',
+                    text: 'Si! È fantastica',
                     status: 'sent'
                     }
                 ],
