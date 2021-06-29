c3aed6a823425d46993964f423c362d64f46d61b
MattiaR <rovitomattia91@gmail.com>
2021-06-29 15:18:23 +0200

    add primo punto (incompleto) Milestone 2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
+//! Milestone 1
+//* Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e  
+//* dall’interlocutore (bianco) assegnando due classi CSS diverse
+//* Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare
+//* nome e immagine di ogni contatto
 
+//! Milestone 2
+//* Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i
+//* messaggi relativi al contatto attivo all’interno del pannello della conversazione
+//* Click sul contatto mostra la conversazione del contatto cliccato
+
+//! Milestone 3
+//* Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
+//* “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+//* Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
+//* un “ok” come risposta, che apparirà dopo 1 secondo.
+
+//! Milestone 4
+//* Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i
+//* contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo
+//* “mar” rimangono solo Marco e Martina)
+
+//! Milestone 5 - opzionale
+//* Cancella messaggio: cliccando sul messaggio appare un menu a tendina che
+//* permette di cancellare il messaggio selezionato
 
 
 
@@ -7,23 +31,6 @@ const app = new Vue ({
     el : "#app",
 
     data : {
-        messages: [
-            {
-            date: '10/01/2020 15:30:55',
-            text: 'Hai portato a spasso il cane?',
-            status: 'sent'
-            },
-            {
-            date: '10/01/2020 15:50:00',
-            text: 'Ricordati di dargli da mangiare',
-            status: 'sent'
-            },
-            {
-            date: '10/01/2020 16:15:22',
-            text: 'Tutto fatto!',
-            status: 'received'
-            }
-        ],
         contacts: [
             {
                 name: 'Giangi',
