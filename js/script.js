7ae259e5cc47243d9947e5b54cbcddea8d15abf8
MattiaR <rovitomattia91@gmail.com>
2021-07-01 12:10:19 +0200

    Milestone 4

--- js/script.js
+++ js/script.js
@@ -236,14 +236,14 @@ const app = new Vue ({
                 this.contacts[this.counter].messages.push(nuovaRisposta)
             }, 1000);
         },
-    },
-    computed : {
         ricerca() {
-            return this.contacts.forEach((element) => {
-                if(this.contacts.visible == true){
-                    return element.name.toLowerCase().includes(this.search.toLowerCase())
-                }  
-            })
+            this.contacts.forEach((element) => {
+                if(element.name.toLowerCase().includes(this.search.toLowerCase())){
+                    element.visible = true;
+                } else{
+                    element.visible = false;
+                }
+            });   
         }
-    }
+    },
 });
